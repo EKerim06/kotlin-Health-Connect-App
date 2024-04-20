13b6b41dd3bbaa1aefec2c4d7956e61417eba87b
Elber Kerim <kerimelber2@gmail.com>
2024-04-20 17:23:08 +0300

    date time bug fixed

--- app/src/main/java/com/example/healt_connect_test_app/features/personalDatas/PersonalDatas.kt
+++ app/src/main/java/com/example/healt_connect_test_app/features/personalDatas/PersonalDatas.kt
@@ -21,16 +21,17 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.LocalDateTime
+import java.time.OffsetTime
 import java.time.ZoneOffset
 
 val dataPermission =
     setOf(
         HealthPermission.getReadPermission(StepsRecord::class),
         HealthPermission.getWritePermission(StepsRecord::class),
-//        HealthPermission.getReadPermission(HeartRateRecord::class),
-//        HealthPermission.getWritePermission(HeartRateRecord::class),
-//        HealthPermission.getReadPermission(SpeedRecord::class),
-//        HealthPermission.getWritePermission(SpeedRecord::class),
+        HealthPermission.getReadPermission(HeartRateRecord::class),
+        HealthPermission.getWritePermission(HeartRateRecord::class),
+        HealthPermission.getReadPermission(SpeedRecord::class),
+        HealthPermission.getWritePermission(SpeedRecord::class),
     )
 
 class PersonalDatas : AppCompatActivity() {
@@ -92,8 +93,24 @@ class PersonalDatas : AppCompatActivity() {
             // permission al ready granted read or write data
             val time = LocalDateTime.now()
             val daysAgo = time.minusDays(10)
+            val instant = daysAgo.toInstant(ZoneOffset.UTC)
             aggregateSteps(
-                healthConnectClient, Instant.now(), Instant.from(daysAgo.atOffset(ZoneOffset.UTC))
+                healthConnectClient,
+                startTime = instant,
+                endTime = Instant.now()
+            )
+
+
+            aggregateHeart(
+                healthConnectClient,
+                startTime = instant,
+                endTime = Instant.now(),
+            )
+
+            aggregateSpeed(
+                healthConnectClient,
+                startTime = instant,
+                endTime = Instant.now(),
             )
 
         } else {
@@ -116,11 +133,58 @@ class PersonalDatas : AppCompatActivity() {
             )
             // The result may be null if no data is available in the time range
             val stepCount = response[StepsRecord.COUNT_TOTAL]
+            println("step count read value : $stepCount")
             stepCountTextView.text = (stepCount ?: "N/a").toString()
         } catch (e: Exception) {
             // Run error handling here
+            println("=====================  aggregateSteps error : $e ")
+
         }
     }
 
+    private suspend fun aggregateHeart(
+        healthConnectClient: HealthConnectClient,
+        startTime: Instant,
+        endTime: Instant
+    ) {
+        try {
+            val response = healthConnectClient.aggregate(
+                AggregateRequest(
+                    metrics = setOf(HeartRateRecord.BPM_AVG),
+                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
+                )
+            )
+            // The result may be null if no data is available in the time range
+            val stepCount = response[HeartRateRecord.BPM_AVG]
+            println("step count read value : $stepCount")
+            heartRateTextView.text = (stepCount ?: "N/a").toString()
+        } catch (e: Exception) {
+            // Run error handling here
+            println("=====================  aggregateHeart error : $e ")
+
+        }
+    }
+
+    private suspend fun aggregateSpeed(
+        healthConnectClient: HealthConnectClient,
+        startTime: Instant,
+        endTime: Instant
+    ) {
+        try {
+            val response = healthConnectClient.aggregate(
+                AggregateRequest(
+                    metrics = setOf(SpeedRecord.SPEED_AVG),
+                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
+                )
+            )
+            // The result may be null if no data is available in the time range
+            val stepCount = response[SpeedRecord.SPEED_AVG]
+            println("step count read value : $stepCount")
+            speedTextView.text = (stepCount ?: "N/a").toString()
+        } catch (e: Exception) {
+            // Run error handling here
+            println("=====================  aggregateSpeed error : $e ")
+        }
+    }
 
 }
